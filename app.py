459c3d56aae34da95d1686c1dbd946ed80169e1e
bakhtsingh14 <bakhtsingh777@gmail.com>
2024-11-06 12:41:01 -0600

    fix: increase the context_length of the model

--- app.py
+++ app.py
@@ -49,7 +49,7 @@ if user_input := st.chat_input("Enter your question here..."):
         for res in client.chat.completions.create(
             model="Hermes-3-Llama-3.1-70B",
             messages=st.session_state.messages,
-            max_tokens=500,
+            max_tokens=13500,
             temperature=0.1,
             stream=True
         ):
